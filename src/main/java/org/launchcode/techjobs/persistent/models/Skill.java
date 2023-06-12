c97c6d6be246455bddc9c5d43f43f8ecb23205a9
John Woolbright <jwoolbright23@gmail.com>
2023-06-12 14:22:24 -0500

    README.md

--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,33 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.Size;
+import org.springframework.core.SpringVersion;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    @Size(max = 100)
+    private String description;
+
+    private String job;
+
+    public Skill (){}
+
+    public  String getDescription() {
+        return description;
+    }
+
+    public void setDescription( String description) {
+        this.description = description;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
 }
+
